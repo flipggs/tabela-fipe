d24701a431affb54a8f5315cfb61e4ace7cf2d82
Felipe Siqueira <flipggs@gmail.com>
2022-05-04 17:46:11 -0300

    add config next-cors

--- pages/api/image.js
+++ pages/api/image.js
@@ -1,6 +1,14 @@
 import { google } from "googleapis";
+import NextCors from "nextjs-cors";
 
 async function handler(req, res) {
+  await NextCors(req, res, {
+    // Options
+    methods: ["GET"],
+    origin: "*",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
+
   const customSearch = google.customsearch("v1");
   if (req.method === "GET") {
     const { q } = req.query;
